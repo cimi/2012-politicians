4d80e4b578a9e227a057b5708d08fe68c0ffae30
Alex Ciminian <alex.ciminian@gmail.com>
2012-11-21 00:19:00 +0200

    refactoring typeahead and html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,5 @@
 define(['jquery', 'politicians'], function($, Politicians) {
+  "use strict"
   // read senate and chamber of deputies data
 
   return {
@@ -17,6 +18,33 @@ define(['jquery', 'politicians'], function($, Politicians) {
         });
       });
       return new Politicians(politicians);
+    },
+    createTypeahead : function (flatData) {
+      // create a typeahead search box to hilight
+      // certain candidates
+      var labels, mapped, that = this;
+      var typeaheadOptions = {
+        source : function (query, process) {
+          labels = []; mapped = {};
+          $.each(flatData, function (idx, politician) {
+            if (politician.name.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
+              labels.push(politician.name);
+              mapped[politician.name] = politician;
+            }
+          });
+          process(labels);
+        },
+        updater : function (name) {
+          var politician = mapped[name];
+          that.chart.unhilight();
+          that.chart.hilight(politician);
+          return name;
+        }
+      };
+      $('#typeaheadSearch').typeahead(typeaheadOptions);
+    },
+    setChart : function (chart) {
+      this.chart = chart;
     }
   }
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,11 +17,16 @@ require(['app', 'pack', 'typeahead'], function(app, PackChart, typeahead) {
   var data = politicians.getSenateByParty();
   var chart = new PackChart(data);
 
+  app.setChart(chart);
+
   // create buttons to update the chart
   // and fill with members from only one
   // party
   var $btnGroup = $('#partyBtnGroup');
-  $('<a href="#" class="btn">Total</a>').click(function (e) { chart.update(data); }).appendTo($btnGroup);
+  $('<a href="#" class="btn">Orice partid</a>')
+      .click(function (e) { 
+        chart.update(data); 
+      }).appendTo($btnGroup);
   $.each(data.children, function (idx, party) {
     var btn = $('<a href="#" class="btn" id="' + party.name + '">' + party.name + ' (' + party.children.length + ')</a>');
     btn.click(function (e) {
@@ -30,26 +35,5 @@ require(['app', 'pack', 'typeahead'], function(app, PackChart, typeahead) {
     $btnGroup.append(btn);
   });
 
-  // create a typeahead search box to hilight
-  // certain candidates
-  var labels, mapped, flatData = politicians.getSenateList(); 
-  var typeaheadOptions = {
-    source : function (query, process) {
-      labels = []; mapped = {};
-      $.each(flatData, function (idx, politician) {
-        if (politician.name.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
-          labels.push(politician.name);
-          mapped[politician.name] = politician;
-        }
-      });
-      process(labels);
-    },
-    updater : function (name) {
-      var politician = mapped[name];
-      chart.unhilight();
-      chart.hilight(politician);
-      return name;
-    }
-  };
-  $('#typeaheadSearch').typeahead(typeaheadOptions);
+  app.createTypeahead(politicians.getSenateList());
 });
